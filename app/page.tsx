be516d642416853c5ecdf66f1a07896d54029d88
roggrt <42749056+roggrt@users.noreply.github.com>
2025-12-16 22:47:24 -0500

    bien

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
+    <div className="relative w-screen h-screen overflow-hidden bg-white dark:bg-black">
+      {/* Imagen para móvil */}
+      <Image
+        src="/responsive.png"
+        alt="Under Construction"
+        fill
+        priority
+        className="object-contain md:hidden"
+      />
+      {/* Imagen para desktop */}
       <Image
         src="/underconstruction.png"
         alt="Under Construction"
         fill
         priority
-        className="object-cover"
+        className="object-contain hidden md:block"
       />
     </div>
   );
